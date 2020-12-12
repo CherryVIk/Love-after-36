95d99954a520cc7ae1e83e9cb2d1f131f13d7f3c
Victoria Boichenko <vik.boichenko@gmail.com>
2020-12-12 13:54:53 +0200

    Initial Commit

---
+++ Love-after-36/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Love-after-36
+//
+//  Created by Victoria Boichenko on 12.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
